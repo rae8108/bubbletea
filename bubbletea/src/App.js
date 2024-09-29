902923aaf044f0be3002844a6447704e27548842
rae8108 <rayanbana01@gmail.com>
2024-09-29 17:54:27 +0100

    installed react

---
+++ bubbletea/src/App.js
@@ -0,0 +1,14 @@
+import Homepage from "./Homepage"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Homepage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
