90ad4f0e002884317a6645cf445920c8c5c1e666
rae8108 <rayanbana01@gmail.com>
2024-09-29 17:54:40 +0100

    created homepage component

---
+++ bubbletea/src/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Homepage.css"
+
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Bubble Tea</h1>
+            <h2>seasonal drinks</h2>
+        </div>
+    )
+}
